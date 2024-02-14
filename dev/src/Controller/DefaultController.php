0a2f93a382029d6ad9e00804c76716520db741ee
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-02-14 15:57:09 -0300

    chore: definindo controller default

---
+++ dev/src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'default')]
+    public function index(): JsonResponse
+    {
+        return $this->json(['message' => 'Hello World!']);
+    }
+}
